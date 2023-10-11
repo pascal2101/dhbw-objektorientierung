e5a640554e567a485abfdd4e24e9865868e291de
pascal2101 <pascal.weber.97@gmail.com>
2023-10-11 13:27:21 +0200

    test

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -5,33 +5,27 @@ class GameWindow : public Gosu::Window
 {
 public:
 
-	GameWindow()
-		: Window(800, 600)
-	{
-		set_caption("Gosu Tutorial mit Git");
-	}
+    GameWindow()
+        : Window(800, 600, true) // Fensterbreite, Fensterhöhe, Vollbildmodus (false für Fenstermodus)
+    {
+        set_caption("Gosu Tutorial mit Git");
+    }
 
-	// Wird bis zu 60x pro Sekunde aufgerufen.
-	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
-	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	void draw() override
-	{
-		graphics().draw_line(
-			10, 20, Gosu::Color::RED,
-			200, 100, Gosu::Color::GREEN,
-			0.0
-		);
-	}
+    void draw() override
+    {
+        graphics().draw_line(
+            10, 20, Gosu::Color::RED,
+            200, 100, Gosu::Color::GREEN,
+            0.0
+        );
+    }
 
-	// Wird 60x pro Sekunde aufgerufen
-	void update() override
-	{
-	}
+    void update() override
+    {
+    }
 };
 
-// C++ Hauptprogramm
 int main()
 {
-	GameWindow window;
-	window.show();
+    
 }
